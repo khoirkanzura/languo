1da4bb706df037bc7c809ada1692747c218573de
gerly <gherly.ylrehg@gmail.com>
2025-12-06 19:34:26 +0700

    fix: format tanggal di main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'routes/routes_manager.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await initializeDateFormatting('id_ID', null);
   runApp(const MyApp());
 }
 
